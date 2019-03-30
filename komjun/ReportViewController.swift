b82d1f77707bc36e06a4c2cc2d241ccd92b8d751
kelfs <kajsafs@gmail.com>
2019-03-30 22:06:38 +0100

    Added new viev and a fab-button for navigation

---
+++ komjun/ReportViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  komjun
+//
+//  Created by Kajsa Sundman on 2019-03-30.
+//  Copyright © 2019 Komjun. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class ReportViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
