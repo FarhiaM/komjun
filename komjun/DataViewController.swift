97de056833e1a6f7f4a4dd2dd647089425a74317
Farhia M <farhia.fcb@gmail.com>
2019-03-30 18:13:51 +0100

    initial commit

---
+++ komjun/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  komjun
+//
+//  Created by Farhia Mohamed on 2019-03-30.
+//  Copyright © 2019 Komjun. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
