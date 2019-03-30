9afce774fb29fd2e1d7df1259e7dca4b8a29b33a
Farhia M <farhia.fcb@gmail.com>
2019-03-30 18:49:07 +0100

    last initial commit maybe

---
+++ komjun/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  komjun
+//
+//  Created by Farhia Mohamed on 2019-03-30.
+//  Copyright © 2019 Komjun. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
